eff44ab1d0b12fe7eb13ef5b5dfb2b0fc2087bd5
Kimmini <imk0980@gmail.com>
2022-06-23 03:08:34 +0900

    v2

--- Address/test.c
+++ Address/test.c
@@ -8,6 +8,10 @@ void add(void);
 void find(void);
 void status(void);
 void delete(void);
+void load(void);
+void save(void);
+void search(void);
+
 
 char *names[BUFFER_MAX];
 char *numbers[BUFFER_MAX];
@@ -28,8 +32,12 @@ int main()
         else if(strcmp(string, "status") == 0)
             status();
         else if(strcmp(string, "delete") == 0)
-            delete();      
-        else
+            delete();    
+        else if(strcmp(string, "load") == 0)
+            load();      
+        else if(strcmp(string, "save") == 0)
+            save();        
+        else if(strcmp(string, "exit") == 0)
             break;
     }
     return 0;
@@ -38,12 +46,19 @@ int main()
 
 void add(void)
 {
+    int i = n-1;
     char number[BUFFER_MAX],name[BUFFER_MAX];
     scanf("%s",name);
     scanf("%s",number);
     
-    names[n] = strdup(name);
-    numbers[n] = strdup(number);
+    while(i>=0 && strcmp(names[i],name) >0)
+    {
+        names[i+1] = names[i];
+        numbers[i+1] = numbers[i];
+        i--
+    }
+    names[i+1] = strdup(name);
+    numbers[i+1] = strdup(number);
     n++;
 
     printf("%s is added completely. \n",name);
@@ -64,20 +79,74 @@ void status(void)
     for(num = 0; num<n; num++)
         printf("%s %s \n",names[num],numbers[num]);
 }
-void delete(void)
+void remove(void)
 {
-    int i = 0;
-    char dnf[BUFFER_MAX];
-    scanf("%s",dnf);
-    for(i = 0;i<n;i++)
-        if(strcmp(dnf,names[i]) == 0)
-            {
-                names[i] == names[n-1];
-                numbers[i] == numbers[n-1];
-            }
+    int index, j;
+    char buf[BUFFER_MAX];
+    /*
+    index = search(buf) //추후 추가!
+    if (index = -1)
+    {
+        printf("No Person named %s exists",buf);
+        return;
+    } 
+    */
+    scanf("%s",buf);
+
+    int j = index;
+    for( ;j<n-1; j++) //빈 칸을 지우고 뒤에 걸 앞으로 땡겨야 함
+    {
+        names[j] = names[j+1];
+        numbers[j] = numbers[j+1]
+    }
+    //n-1보다 작은 동안인 이유 j가 n-1까지 되어버리면 j+1은 n이 되는데 names[n]은 빈칸
+    //names[0] ~ names[n-1] 까지 있기 때문
+    
+
     n--;
 }
 
+void load(void)
+{
+    char fileName[NAME_MAX];
+    char buf1[BUFFER_MAX],buf2[BUFFER_MAX];
+    FILE *fp = fopen(fileName, "r");
+    if(fp = NULL)
+    {
+        printf("load failed\n");
+        return;
+    }
+    while(fscanf(fp,"%s",buf1) != EOF) //'\n'을 종료로 간주 -> 이름만 저장!
+    {
+        fscanf(fp, "%s",buf2); // '\n'을 종료로 간주 -> 번호만 저장!
+        names[n] = strdup(buf1);
+        numbers[n] = strdup(buf2);
+        n++
+    }
+    fclose(fp);
+}
+void load(void);
+{
+    int i;
+    char tmp[BUFFER_MAX];
+    char fileName[BUFFER_MAX];
+
+    scanf("%s",tmp);
+    scanf("%s",fileName);
+
+    FILE *fp = fopen(fileName, "w");
+    if(fp = NULL)
+    {
+        printf("load failed\n");
+        return;
+    }
+
+    for(i = 0; i<n; i++)
+        fprintf(fp,"%s %s\n",names[i],numbers[i]);
+    
+    fclose(fp);
+}
+void save(void);
 
 
 
